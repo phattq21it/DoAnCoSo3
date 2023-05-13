1a0e67996526604e76d2009bc037bdfbe8a1c90b
phattq21it <106509909+phattq21it@users.noreply.github.com>
2023-05-13 12:15:28 +0700

    - sua loi do hang của toi

---
+++ adminapp/src/main/java/com/example/adminapp/WelcomesActivity.java
@@ -0,0 +1,13 @@
+package com.example.adminapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcomes);
+    }
+}
